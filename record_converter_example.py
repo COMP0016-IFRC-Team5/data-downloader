c52590145ce20e9007bb841204cb9974e78d2919
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-09 15:19:50 +0000

    fix: directory

--- record_converter_example.py
+++ record_converter_example.py
@@ -3,5 +3,5 @@ import record_converter
 
 if __name__ == '__main__':
     converter = record_converter.RecordsConverter()
-    converter.set_data_folder("../data")
+    converter.set_data_folder("./data")
     converter.start_conversion()
--- xml_example.py
+++ xml_example.py
@@ -1,5 +1,4 @@
 import xml_downloader
 
 if __name__ == '__main__':
-    xml_downloader.start_process(target_dir="./data",
-                                 clean_zip=False)
+    xml_downloader.start_process(target_dir="./data", clean_zip=False)
